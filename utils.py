095236bded69200183bdbeac73fbe0e06ad488fc
Jesse Andrews <anotherjesse@gmail.com>
2025-04-05 15:56:14 -0400

    re-add seed

--- utils.py
+++ utils.py
@@ -31,6 +31,7 @@ def seed_everything(seed: int) -> None:
     """
     random.seed(seed)
     np.random.seed(seed)
+    torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
     
